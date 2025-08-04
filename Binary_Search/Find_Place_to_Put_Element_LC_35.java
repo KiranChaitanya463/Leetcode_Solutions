36cee447cc1a86a8133137f81ab87c7ef097a5ae
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-04 06:15:59 +0530

    search positon to place the element lc 35 using lower bound'

---
+++ Binary_Search/Find_Place_to_Put_Element_LC_35.java
@@ -0,0 +1,8 @@
+package Binary_Search;
+
+public class Find_Place_to_Put_Element_LC_35 {
+    /* 
+     * we have to find the position which the element can be placed in a sorted array 
+     * we can use the lower bound approach where it puts the element after a certain target or if the target exists it can place anywhere might be put it ata start of that sequence of that occurance element.
+     */
+}
