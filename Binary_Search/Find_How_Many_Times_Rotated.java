7f247b97b908b11fd6cf309198ca7caf0c2e9413
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-04 06:35:55 +0530

    number of times the array rotated using the min in roated sorted array

---
+++ Binary_Search/Find_How_Many_Times_Rotated.java
@@ -0,0 +1,9 @@
+package Binary_Search;
+
+public class Find_How_Many_Times_Rotated {
+    
+    /* 
+     * we have to find how many times does array rotated 
+     * if we can actually find the min in this array (rotated sorted array) that index will be the number of times it rotated.
+     */
+}
