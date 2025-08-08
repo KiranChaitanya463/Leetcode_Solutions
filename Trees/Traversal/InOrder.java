0539909db24194fcb6ffafef77d12081dbb464b7
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-08 06:45:35 +0530

    inorder and preorder with itterative

--- Trees/Traversal/InOrder.java
+++ Trees/Traversal/InOrder.java
@@ -2,6 +2,7 @@ package Trees.Traversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import Trees.TreeNode;
 
@@ -10,8 +11,29 @@ public class InOrder {
 }
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+
+        //recursive
+        // List<Integer> list=new ArrayList<>();
+        // traverse(root,list);
+        // return list;
+
+        // itterative
         List<Integer> list=new ArrayList<>();
-        traverse(root,list);
+        if(root==null) return list;
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+                if(st.isEmpty()) break;
+                node=st.pop();
+                list.add(node.val);
+                node=node.right;
+            }
+        }
         return list;
     }
     public void traverse(TreeNode node,List<Integer> list){
--- Trees/Traversal/PreOrder.java
+++ Trees/Traversal/PreOrder.java
@@ -2,6 +2,7 @@ package Trees.Traversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import Trees.TreeNode;
 
@@ -10,8 +11,24 @@ public class PreOrder{
 }
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+
+        // recursive
+        // List<Integer> list=new ArrayList<>();
+        // traverse(root,list);
+        // return list;
+
+        // itterative
         List<Integer> list=new ArrayList<>();
-        traverse(root,list);
+        if(root==null) return list;
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            root=st.pop();
+            // like add root see if right exists and then push and then left exists then push left and for next itteration it will be considered as root.
+            list.add(root.val);
+            if(root.right!=null) st.push(root.right);
+            if(root.left!=null) st.push(root.left);
+        }
         return list;
     }
     public void traverse(TreeNode node,List<Integer> list){
