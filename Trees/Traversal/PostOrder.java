df446a4fb3ecf87b7c362be01278deb2a9b7152b
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-09 20:30:53 +0530

    postorder itterative approach using 2 stack

--- Trees/Traversal/PostOrder.java
+++ Trees/Traversal/PostOrder.java
@@ -2,6 +2,7 @@ package Trees.Traversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import Trees.TreeNode;
 
@@ -11,8 +12,29 @@ public class PostOrder {
 
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+
+        // recursive
+        // List<Integer> list=new ArrayList<>();
+        // traverse(root,list);
+        // return list;
+
+        //itterative - using 2 stack
         List<Integer> list=new ArrayList<>();
-        traverse(root,list);
+        if(root==null) return list;
+        TreeNode node=root;
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
+
+        st1.push(node);
+        while(!st1.isEmpty()){
+            node=st1.pop();
+            st2.push(node);
+            if(node.left!=null) st1.push(node.left);
+            if(node.right!=null) st1.push(node.right);
+        }
+        while(!st2.isEmpty()){
+            list.add(st2.pop().val);
+        }
         return list;
     }
     public void traverse(TreeNode node,List<Integer> list){
