89d51ee8aa8ec6ef3258d290019c84fbf83d8fe6
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-12 08:22:30 +0530

    check if the two trees are identical or not

---
+++ Trees/Problems/CheckIfIdentical.java
@@ -0,0 +1,17 @@
+package Trees.Problems;
+
+import Trees.TreeNode;
+
+public class CheckIfIdentical {
+    // we can find the two trees are identical or not by doing the traversal on both the trees at same time at any point the condition false or the values not equal the return false
+}
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null || q==null){
+            return (p==q);
+        }
+        return (p.val==q.val) &&
+                isSameTree(p.left,q.left) &&
+                isSameTree(p.right,q.right);
+    }
+}
