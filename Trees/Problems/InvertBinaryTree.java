4af0a459f59ece95cf8f370d0e2c04f38b097e25
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-22 19:01:08 +0530

    invert a binary tree

---
+++ Trees/Problems/InvertBinaryTree.java
@@ -0,0 +1,19 @@
+package Trees.Problems;
+import Trees.TreeNode;
+
+public class InvertBinaryTree {
+
+    // so exchanging the node references while traversing 
+    // so when the exchange happen the below subtree will also go with not just that node (cause i stuck with that question for about 20 min)
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
