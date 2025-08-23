970cfe34689c2e3dd6cf2c7743c93137ebe1029f
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-23 11:45:40 +0530

    flattening the binary tree to linked list

---
+++ Trees/Problems/FlattenBTtoLL.java
@@ -0,0 +1,32 @@
+package Trees.Problems;
+
+import Trees.TreeNode;
+
+//lc114
+public class FlattenBTtoLL {
+    public void flatten(TreeNode root) {
+
+        // so the thing is i tried to create the new nodes and try to attach to the root node
+        // but it wont work because when an object is sent via the method it sends the copy 
+        // of the reference and we modify the contents with that reference within the object
+        // but we can change that particular reference to another object rather it will point
+        // to new object and it does not change the original object whcih having the original 
+        // reference. so we have change that in place
+
+        if(root==null) return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        TreeNode left=root.left;
+        TreeNode right=root.right;
+
+        root.left=null;
+        root.right=left;
+
+        TreeNode curr=root;
+        while(curr.right!=null) curr=curr.right;
+
+        curr.right=right;
+    }
+}
