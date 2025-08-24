2d39458b81645ed997a9b4f34e9c03298def2f18
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-08-24 19:02:04 +0530

    counting number of nodes in log2n time cause we have the complete binary tree (not perfect)

--- Trees/Problems/CountNodes.java
+++ Trees/Problems/CountNodes.java
@@ -7,7 +7,26 @@ public class CountNodes {
     public int countNodes(TreeNode root) {
         if(root==null) return 0;
         if(root.left==null) return 1;
-        return count(root);
+        // this approach takes a time of O(N) and space of O(H) and they are asking to take less than O(N)
+        // return count(root);
+
+        // this takes O(Log^2N)
+        return findCount(root);
+
+    }
+    public int findCount(TreeNode root){
+        int lh=leftHeight(root);
+        int rh=rightHeight(root);
+        if(lh==rh) return (int)Math.pow(2,lh)-1;
+        return 1+findCount(root.left) + findCount(root.right);
+    }
+    public int leftHeight(TreeNode root){
+        if(root==null) return 0;
+        return 1 + leftHeight(root.left);
+    }
+    public int rightHeight(TreeNode root){
+        if(root==null) return 0;
+        return 1 + rightHeight(root.right);
     }
     public int count(TreeNode root){
         if(root==null) return 0;
