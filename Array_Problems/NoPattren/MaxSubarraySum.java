c9f4b09fc734087bcb2a2af1f52933eedc463caa
KiranChaitanya463 <kiranchaitanya463@gmail.com>
2025-09-14 13:14:42 +0530

    max subarraysum

---
+++ Array_Problems/NoPattren/MaxSubarraySum.java
@@ -0,0 +1,19 @@
+package Array_Problems.NoPattren;
+
+//lc53
+public class MaxSubarraySum {
+    public int maxSubArray(int[] nums) {
+        int maxSum=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i: nums){
+            sum+=i;
+            if(sum>maxSum){
+                maxSum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxSum;
+    }
+}
